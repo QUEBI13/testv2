44368f80da2a2e852724bd06cf0cce981b874117
KIMMEE <kimmeecubinar@yahoo.com>
2018-03-15 16:28:51 +0700

    make another page

---
+++ js/nav.js
@@ -0,0 +1,8 @@
+$(window).scroll(function(){
+	if($('.main-navmenu2').offset().top > 50){
+		$('.navbar-fixed-top').addClass('top-nav-collapse');
+		{
+			else{
+				$('.navbar-fixed-top').removeClass('top-nav-collapse');
+			}
+	});
